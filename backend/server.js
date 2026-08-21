a8eec622a08a123b8edac521a03dac32ba64cfab
shiniefg2 <thalissilva.souza@aluno.educa.go.gov.br>
2026-08-21 12:18:35 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
@@ -146,6 +146,8 @@ app.get("/medicacoes", (req, res) => {
 });
 
 // START
-app.listen(3000, () => {
-  console.log("🏥 Hospital Pro rodando em http://localhost:3000");
+const PORT = process.env.PORT
+        || 3000;
+app.listen(PORT, () => {
+  console.log(`Porta ${POST} `);
 });
